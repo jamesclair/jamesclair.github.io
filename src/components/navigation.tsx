a1ee6102e8938374b78165351da5b1624aabd63b
jamesclair <clair.james88@gmail.com>
2024-01-25 21:07:32 -0800

    feat: add collapsing support to navbar

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,24 +1,40 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from "react-router-dom";
+import styles from './navigation.module.css'
 import '../index.scss';
 
 function Navigation() {
     return (
-        <Navbar collapseOnSelect expand="lg">
-            <Container>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="gradient-text">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
-                        <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
-                        <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+        <Navbar expand='lg'>
+            <Container fluid>
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+                <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-lg`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+                    placement="end"
+                >
+                    <Offcanvas.Header closeButton className={styles['nav-header']}>
+                        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                            Menu
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="gradient-text">
+                                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                                <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+                                <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
+                                <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
             </Container>
-        </Navbar>
+        </Navbar >
     );
 }
 
