77c83a91efe558314a9aa779ae9d612daa25aacc
jamesclair <clair.james88@gmail.com>
2024-09-23 13:37:47 -0500

    feat(timeline): add dependencies

---
+++ src/components/timeline.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent, { timelineOppositeContentClasses } from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+
+import { useTheme, useMediaQuery, Typography } from '@mui/material';
+
+
+import TerminalIcon from '@mui/icons-material/Terminal';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import MediationIcon from '@mui/icons-material/Mediation';
+import MoveUpIcon from '@mui/icons-material/MoveUp';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import ArchitectureIcon from '@mui/icons-material/Architecture';
+import WarningIcon from '@mui/icons-material/Warning';
+
+import { styled } from '@mui/system';
+import styles from './timeline.module.css'
+
+import Layout from './layout';
+
+
