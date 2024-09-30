182a2ec7b3279465ff2745ae35f5a095eb18d89b
jamesclair <clair.james88@gmail.com>
2024-09-30 13:51:31 -0500

    feat(portfolio): add portfolio page

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,9 @@
+import CustomizedTimeline from '../components/timeline'
+
+const Portfolio: React.FC = () => {
+    return (
+        <CustomizedTimeline></CustomizedTimeline>
+    )
+}
+
+export default Portfolio
